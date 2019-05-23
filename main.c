1bd7c6e7e7298817388b5534a7ec9d4f2cf6f627
zengxianyugithub <1225508738@qq.com>
2019-05-23 17:05:17 +0800

    main.c
    再加一个版本

---
+++ main.c
@@ -0,0 +1,10 @@
+
+/****
+TCP Address: tcp://mjjba76.mqtt.iot.gz.baidubce.com:1883
+SSL Address: ssl://mjjba76.mqtt.iot.gz.baidubce.com:1884
+WSS Address: wss://mjjba76.mqtt.iot.gz.baidubce.com:443
+name: mjjba76/demoiot_1
+key: qy3hywwxvc3dr9cn
+*/
+
+//mqtt iot
